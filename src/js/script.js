2c3979bbc1ae4c069dbb3bd89bc678e80f598b38
Anthony Roux <roux.anthony@hetic.net>
2020-07-12 22:52:07 +0200

    create structure project

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+function toggle(elemID) {
+  const elem = document.getElementById(elemID);
+
+  if (elem.style.display === 'block') {
+    elem.style.display = "none";
+  } else {
+    elem.style.display = 'block';
+  }
+}
+
+console.log("coucou");
+console.log("crotte");
+console.log("doja");
